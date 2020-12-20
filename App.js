a5246930bc7b06d0a41adbddcf5b7e75193ec1a3
Alejandro Murawczik <amurawczik@almacorp.es>
2020-12-20 13:35:27 +0100

    MODIFY data

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Dimensions, FlatList } from 'react-native';
+
+// const {width} = Dimensions.get('window').width
 
 export default function App() {
+  const [ users, setUsers ] = useState([])
+  const [ loading, setLoading ] = useState(true)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data)
+        setLoading(false)
+      }) 
+  }, []);
+
+  if(loading){
+    return <View style={styles.center}>
+      <Text>Cargando...</Text>
+    </View>
+  }
   return (
     <View style={styles.container}>
-      <Text>Hola mundo!</Text>
+      <FlatList 
+        data={users}
+        renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
+        keyExtractor={item => String(item.id)}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +36,21 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+    paddingTop: 30,
   },
+  item: {
+    padding: 10,
+    fontSize: 22,
+    height: 50,
+    borderBottomWidth: 1,
+    borderBottomColor: '#222',
+   },
+   center: {
+     flex: 1,
+     justifyContent: 'center',
+     alignItems: 'center'
+   },
 });
